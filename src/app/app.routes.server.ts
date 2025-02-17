960f3596fe25562e7e613c6ea673dcb29c15e9dd
shonez-mohamed <shoneezmohamed8@gmail.com>
2025-02-17 04:13:31 +0200

    serverRoutes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,21 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'home', 
+    renderMode: RenderMode.Server,
+  },
+  
+  {
+    path:'address/:cartID', 
+    renderMode: RenderMode.Server,
+  },
+  
+  {
+    {path:'productDetails/:productID' , 
+    renderMode: RenderMode.Server,
+  },
+  
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
